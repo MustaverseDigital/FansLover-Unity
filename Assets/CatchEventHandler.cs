0382a10c3d1a2dbd4405c52f6beccec23fcb4e7e
ATuan <jain60501@gmail.com>
2024-10-13 16:37:39 +0800

    added Story line and LLM place holder

--- Assets/CatchEventHandler.cs
+++ Assets/CatchEventHandler.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 
 public class CatchEventHandler : MonoBehaviour
 {
+    private int _replyTimes = 0;
+
     private void Start()
     {
         var variableManager = Engine.GetService<CustomVariableManager>();
         variableManager.OnVariableUpdated += OnVariableUpdated;
+        variableManager.SetVariableValue("LLM", "This is AI Tina Replying");
     }
 
     private void OnVariableUpdated(CustomVariableUpdatedArgs obj)
     {
         Debug.Log($"{obj.Name} {obj.Value} {obj.InitialValue}");
+        if (obj.Name.Equals("PlayerReply"))
+        {
+            AddAiTinaReply();
+        }
+    }
+
+    private void AddAiTinaReply()
+    {
+        _replyTimes++;
+        if (_replyTimes > 2)
+        {
+            Engine.GetService<CustomVariableManager>()
+                .SetVariableValue("loadingStoryID", "1");
+        }
+
+        Engine.GetService<CustomVariableManager>()
+            .SetVariableValue("LLM", $"This is AI Tina Replying {_replyTimes}");
     }
 }
